58ea922a3b39df2be55775bc4ea4933870c82860
Jose Hurtado <jose.alejandro.hurtado@correounivalle.edu.co>
2022-02-17 23:00:42 -0500

    Update CajaLayout.js

--- src/components/layouts/CajaLayout.js
+++ src/components/layouts/CajaLayout.js
@@ -1,9 +1,21 @@
 import React from 'react'
-import { Heading, SimpleGrid } from '@chakra-ui/react'
+import { Heading, SimpleGrid, Flex, Spacer, Square, Box } from '@chakra-ui/react'
 
 const CajaLayout = () => {
     return (
-        <div>
+        <div id='main' style={{opacity: '1', textAlign:'center'}}>
+            <Heading size='4xl' margin='1.2em 0'>Cajas</Heading>
+            <SimpleGrid columns={2} spacing={10}>
+                <Flex color= 'white'>
+                    <Square bg='white' size='150px'>
+                        1
+                    </Square>
+                    <Spacer />
+                    <Box p='4'bg='white'>
+                        Atiende
+                    </Box>
+                </Flex>
+            </SimpleGrid>
             
         </div>
     )
