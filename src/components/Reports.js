3c8f482e342cfe7463bd22bffebe14b55e87fcee
Jose Hurtado <jose.alejandro.hurtado@correounivalle.edu.co>
2022-03-18 08:07:59 -0500

    Update Reports.js

--- src/components/Reports.js
+++ src/components/Reports.js
@@ -130,7 +130,7 @@ const Reports = () => {
         labels: Object.keys(vips),
         datasets: [
           {
-            label: "Cajas de la sede",
+            label: "Cantidad de turnos VIP",
             data: Object.values(vips),
             borderColor: "rgb(53, 162, 235)",
             backgroundColor: "rgba(53, 162, 235, 0.4)",
